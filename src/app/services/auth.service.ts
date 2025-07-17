6d875b5f910957ad1839b51d51a478bec7137fa5
schneiderLukas <schneiderbauerlukas@gmail.com>
2025-07-17 22:28:55 +0200

    auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { Router } from '@angular/router';
+import { catchError, map, Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private api: ApiService, private router: Router) { }
+
+  login(credentials: {username: string, password: string}): Observable<any> {
+    return this.api.post('login', credentials);
+  }
+
+  register(data: {username: string, password: string}): Observable<any> {
+    return this.api.post('register', data);
+  }
+
+  verifyToken(): Observable<any> {
+    const token = this.getToken();
+    if (!token) return of(false);
+
+    return this.api.get<any>('verify-token').pipe(
+      map(() => true),
+      catchError(() => of(false)))
+  }
+
+  logout(): void {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.getToken();
+  }
 }
