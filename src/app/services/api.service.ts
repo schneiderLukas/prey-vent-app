aa9d8056ef779cb78bd0f2a344362561284f8556
schneiderLukas <schneiderbauerlukas@gmail.com>
2025-07-17 22:33:40 +0200

    api service update, get auth headers

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../environments/environment.development'; // Adjust the import based on your environment setup
 
 // @Injectable({
@@ -12,20 +12,33 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
+  private getAuthHeaders(token?: string): HttpHeaders {
+    const jwt = token || localStorage.getItem('token');
+    return new HttpHeaders({
+      'Authorization': jwt ? `Bearer ${jwt}` : '',
+      'Content-Type': 'application/json'
+    });
+
+  } 
+
   get<T>(endpoint: string): Observable<T> {
-    return this.http.get<T>(`${this.url}/${endpoint}`);
+    return this.http.get<T>(`${this.url}/${endpoint}`, {
+      headers: this.getAuthHeaders()});
   }
 
   post<T>(endpoint: string, body: any): Observable<T> {
-    return this.http.post<T>(`${this.url}/${endpoint}`, body);
+    return this.http.post<T>(`${this.url}/${endpoint}`, body, {
+      headers: this.getAuthHeaders()});
   }
 
   put<T>(endpoint: string, body: any): Observable<T> {
-    return this.http.put<T>(`${this.url}/${endpoint}`, body);
+    return this.http.put<T>(`${this.url}/${endpoint}`, body, {
+      headers: this.getAuthHeaders()});
   }
 
   delete<T>(endpoint: string): Observable<T> {
-    return this.http.delete<T>(`${this.url}/${endpoint}`);
+    return this.http.delete<T>(`${this.url}/${endpoint}`, {
+      headers: this.getAuthHeaders()});
   }
   
 }
