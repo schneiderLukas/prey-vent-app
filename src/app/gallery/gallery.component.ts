144a8b2b8f2e5762f33dc0138723d0a9c77438fb
schneiderLukas <schneiderbauerlukas@gmail.com>
2025-10-01 23:24:21 +0200

    functionality

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GalleryService, Album } from '../services/gallery.service';
+import { Lightbox, LightboxModule} from 'ngx-lightbox';
 
 @Component({
   selector: 'app-gallery',
-  imports: [],
+  imports: [CommonModule, LightboxModule],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css'
 })
 export class GalleryComponent {
+
+  albums = signal<Album[]>([]); // Signal holding your albums
+
+
+  constructor(private lightbox: Lightbox, private galleryService: GalleryService) { }
+
+
+  ngOnInit(): void {
+    this.albums.set(this.galleryService.loadAlbums());
+  }
+
+  open(index: number): void {
+    this.lightbox.open(this.albums(), index);
+  }
+
+  close(): void {
+    this.lightbox.close();
+  }
 
 }
--- src/app/services/gallery.service.ts
+++ src/app/services/gallery.service.ts
@@ -1,12 +1,44 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import { ApiService } from './api.service';
+
+export interface Album {
+  src: string;
+  thumb: string;
+  caption: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class GalleryService {
 
-  constructor(private api: ApiService) { 
+  albums = signal<Album[]>([]); // Signal holding your albums
+  
+  constructor(private api: ApiService) { }
     
+
+  loadAlbums() : Array<Album> {
+    return [
+      {
+        src: 'https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png',
+        thumb: 'https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png',
+        caption: 'Cute Kitten 1'
+      },
+      {
+        src: 'https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png',
+        thumb: 'https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png',
+        caption: 'Cute Kitten 1'
+      },
+      {
+        src: 'https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png',
+        thumb: 'https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png',
+        caption: 'Cute Kitten 1'
+      },
+      {
+        src: 'https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png',
+        thumb: 'https://upload.wikimedia.org/wikipedia/commons/b/b6/Image_created_with_a_mobile_phone.png',
+        caption: 'Cute Kitten 1'
+      }
+    ]
   }
 }
