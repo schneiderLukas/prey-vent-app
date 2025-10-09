6fb3cc51ca407cb653d7e75dd95a51dfa64fc460
schneiderLukas <schneiderbauerlukas@gmail.com>
2025-10-09 19:02:52 +0200

    added connect to wifi function

--- src/app/services/network.service.ts
+++ src/app/services/network.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
-import { catchError, map, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 
 interface NetworkInfo {
   ssid: string;
   signal: number;
   secure: boolean;
+}
+interface Credentials {
+  ssid: string;
+  password: string;
+}
+interface ConnectResponse {
+  success: boolean;
+  message?: string;
 }
 
 @Injectable({
@@ -16,12 +24,37 @@ export class NetworkService {
   constructor(private api: ApiService) { }
 
   getAvailableNetworks() {
-    return this.api.get<{ availableNetworks: NetworkInfo[]}>('wifi-networks').pipe(
+    return this.api.get<{ availableNetworks: NetworkInfo[] }>('wifi-networks').pipe(
       catchError(err => {
         console.error('Error fetching networks', err);
         return of({ availableNetworks: [] });
-    }),
-    map(response => response.availableNetworks)
-  );
+      }),
+      map(response => response.availableNetworks)
+    );
+  }
+  connectToWifi(Credentials: Credentials): Observable<ConnectResponse> {
+    const formData = new FormData();
+    formData.append('ssid', Credentials.ssid);
+    formData.append('password', Credentials.password);
+    return new Observable<ConnectResponse>((observer) => {
+      this.api.post<ConnectResponse>('wifi-connect', formData).subscribe({
+        next: (response) => {
+          const connectResponse: ConnectResponse = {
+            success: response.success,
+            message: "Connected successfully"
+          };
+          observer.next(connectResponse);
+          observer.complete();
+        },
+        error: () => {
+          const connectResponse: ConnectResponse = {
+            success: false,
+            message: "Failed to connect"
+          };
+          observer.next(connectResponse);
+          observer.complete();
+        }
+      });
+    });
   }
 }
