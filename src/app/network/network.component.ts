3f33dce4fa28ce5bf32fe227fcae43c92ce947ce
schneiderLukas <schneiderbauerlukas@gmail.com>
2025-10-09 19:10:13 +0200

    created component

---
+++ src/app/network/network.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-network',
+  imports: [],
+  templateUrl: './network.component.html',
+  styleUrl: './network.component.css'
+})
+export class NetworkComponent {
+
+}
