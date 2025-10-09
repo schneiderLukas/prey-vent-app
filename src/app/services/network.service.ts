581b518d9f26933750294521dbb984cc17269cda
schneiderLukas <schneiderbauerlukas@gmail.com>
2025-10-09 18:53:50 +0200

    added function to get available networks

--- src/app/services/network.service.ts
+++ src/app/services/network.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { catchError, map, of } from 'rxjs';
+
+interface NetworkInfo {
+  ssid: string;
+  signal: number;
+  secure: boolean;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class NetworkService {
 
-  constructor() { }
+  constructor(private api: ApiService) { }
+
+  getAvailableNetworks() {
+    return this.api.get<{ availableNetworks: NetworkInfo[]}>('wifi-networks').pipe(
+      catchError(err => {
+        console.error('Error fetching networks', err);
+        return of({ availableNetworks: [] });
+    }),
+    map(response => response.availableNetworks)
+  );
+  }
 }
