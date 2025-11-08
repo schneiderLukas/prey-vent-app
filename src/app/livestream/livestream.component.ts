86efce8d357302077385cac2d4721be65892aa2b
schneiderLukas <schneiderbauerlukas@gmail.com>
2025-11-08 15:27:02 +0100

    live-stream component

--- src/app/livestream/livestream.component.ts
+++ src/app/livestream/livestream.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { LivestreamService } from '../services/livestream.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-livestream',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './livestream.component.html',
   styleUrl: './livestream.component.css'
 })
-export class LivestreamComponent {
+export class LivestreamComponent implements OnInit, OnDestroy {
+  imageUrl: any;
+  private intervalId: any;
 
+  constructor(private livestreamService: LivestreamService) {}
+
+  ngOnInit(): void {
+    this.refreshImage();
+    this.intervalId = setInterval(() => {this.refreshImage(); }, 100);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId);
+  }
+
+  refreshImage(): void {
+    this.livestreamService.getLiveImage().subscribe(url => this.imageUrl = url);
+  }
 }
