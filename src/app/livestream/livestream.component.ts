f6b322671f48381933884bf642ee3de1e40fb705
schneiderLukas <schneiderbauerlukas@gmail.com>
2025-11-08 14:56:38 +0100

    created live-stream-component

---
+++ src/app/livestream/livestream.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-livestream',
+  imports: [],
+  templateUrl: './livestream.component.html',
+  styleUrl: './livestream.component.css'
+})
+export class LivestreamComponent {
+
+}
